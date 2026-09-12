5b9c851c821ab792eb45a83318564d36f1f5d7cf
Sitek25 <sitek2025@gmail.com>
2026-09-12 17:56:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sitek25/Sitek25.github.io'})
+  base: './', 
+})
